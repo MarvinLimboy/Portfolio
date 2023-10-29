c45336ccf130ed34597f63e2ecfe2fc608a92c53
Marvin Limboy <55020084+MarvinLimboy@users.noreply.github.com>
2023-10-29 09:33:32 +0800

    deploy 2

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. hello from Canada
         </p>
         <a
           className="App-link"
